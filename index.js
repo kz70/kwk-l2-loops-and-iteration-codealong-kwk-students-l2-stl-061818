b2d9d0dd9b0ca9ec0fdb5e503d308cb78b764797
Kailin Zhang <kailin11511@gmail.com>
2018-06-21 16:33:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Code your solutions in this file
 function tailsNeverFails() {
   let tails = 0;
+
+  while (Math.random() >= 0.5) {
+  tails++
+  }
+return `You got ${tails} tails in a row`;
   
-  while (Math.random() >= 0.5)
 }

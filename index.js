dbd71b4974a9379820c4213e7450c3c571e04b47
Kailin Zhang <kailin11511@gmail.com>
2018-06-21 16:35:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Code your solutions in this file
+
 function tailsNeverFails() {
   let tails = 0;
 
   while (Math.random() >= 0.5) {
   tails++
-  }
+}
   
 console.log(`You got ${tails} tails in a row`);
 }
